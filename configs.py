65fde62a6d319cbc009331165ad909b64c04d94d
arifrozex <89902391+arifrozex@users.noreply.github.com>
2021-09-04 11:24:12 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,23 +19,18 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
-Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+silahkan jika anda mau mengunkan bot ini!
+yang ingin memesan bot seerti ini anda bisa langsung hubungi developer.
 🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
-📝 **Language:** [Python3](https://www.python.org)
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
-📡 **Hosted on:** [Heroku](https://heroku.com)
-🧑🏻‍💻 **Developer:** @melna21
-👥 **Chanel Utama:** [Linux Repositories](https://t.me/freesini)
-📢 **Komik dewasa Channel:** [Discovery Projects](https://t.me/komikdewasa18)
+🧑🏻‍💻 **Developer:** @ownersri
 """
 	ABOUT_DEV_TEXT = f"""
 🧑🏻‍💻 **Developer:** @melna21
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
-[Donate Now](https://www.paypal.me) (PayPal)
+Terimakasih sudah menggunkan jasa kami. ingat semua file bisa kecuali foto. tolong Gunakan Hal yang Positif.
+jika ada kekurangan dalam bot ini anda bisa langsung hubungi admin klik saja lapor
+[lapor](https://t.me/ownersri) (lapor)
 """
 	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+Hi, [{}](tg://user?id={})\n\nThis is Permanent **link tempat file**.
+silakan kirim file dan video anda di bot ini, jangan kirim foto ya. terimaksih telah mendukung! cek **tentang bot** di bawah ini.
 """
